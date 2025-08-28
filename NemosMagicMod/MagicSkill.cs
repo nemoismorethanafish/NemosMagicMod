1925ed5bcae4c7fbfff2aba51596ea14aabec2a2
nemom <nemoismorethanafish@gmail.com>
2025-08-28 14:08:03 -0500

    Professions are fixed. They still don't do anything.

--- NemosMagicMod/MagicSkill.cs
+++ NemosMagicMod/MagicSkill.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
 using System.Collections.Generic;
 using static SpaceCore.Skills;
@@ -16,25 +15,56 @@ namespace MagicSkill
                 100, 300, 600, 1000, 1500, 2200, 3000, 4000, 5000, 6000
             };
 
-            // Assign both the main icon and skill page icon
             Texture2D icon = NemosMagicMod.ModEntry.MagicSkillIcon;
             this.Icon = icon;
-            this.SkillsPageIcon = icon; //change this to fix the size issue on the skills page
+            this.SkillsPageIcon = icon;
+
+            // Create the professions
+            var prof1 = new CantripFocusProfession(this);
+            var prof2 = new ArcaneSurgeProfession(this);
+
+            // Add the profession pair at level 5
+            this.ProfessionsForLevels.Add(new ProfessionPair(5, prof1, prof2, null));
+
+            // Add the individual professions to the skill
+            this.Professions.Add(prof1);
+            this.Professions.Add(prof2);
         }
 
         public override string GetName() => "Magic";
 
-        public override List<string> GetExtraLevelUpInfo(int level)
-        {
-            return new()
-            {
-                $"Magic bonus: {5 * level}"
-            };
-        }
+        public override List<string> GetExtraLevelUpInfo(int level) =>
+            new() { $"Magic bonus: {5 * level}" };
 
-        public override string GetSkillPageHoverText(int level)
+        public override string GetSkillPageHoverText(int level) =>
+            $"Level {level} Magic bonus: {5 * level}";
+    }
+
+    public class CantripFocusProfession : Skill.Profession
+    {
+        public CantripFocusProfession(Skill parentSkill)
+            : base(parentSkill, "CantripFocus") { }
+
+        public override string GetName() => "Cantrip Focus";
+        public override string GetDescription() => "Choose this profession for flavor only (does nothing).";
+
+        public override void DoImmediateProfessionPerk()
         {
-            return $"Level {level} Magic bonus: {5 * level}";
+            // No functionality for now
+        }
+    }
+
+    public class ArcaneSurgeProfession : Skill.Profession
+    {
+        public ArcaneSurgeProfession(Skill parentSkill)
+            : base(parentSkill, "ArcaneSurge") { }
+
+        public override string GetName() => "Arcane Surge";
+        public override string GetDescription() => "Choose this profession for flavor only (does nothing).";
+
+        public override void DoImmediateProfessionPerk()
+        {
+            // No functionality for now
         }
     }
 }
--- NemosMagicMod/Professions.cs
+++ NemosMagicMod/Professions.cs
@@ -1,36 +1,15 @@
-﻿using StardewValley;
-using System;
+﻿using static SpaceCore.Skills;
 
-public enum MagicProfession
+public class CantripFocusProfession : Skill.Profession
 {
-    None = 0,
-    WindSpiritCantripProfession = 1,
-    FireballCantripProfession = 2,
-}
+    public CantripFocusProfession(Skill parentSkill)
+        : base(parentSkill, "CantripFocus") { }
 
-public static class ProfessionManager
-{
-    private const string ProfessionKey = "NemosMagicMod.Profession";
+    public override string GetName() => "Cantrip Focus";
+    public override string GetDescription() => "Replace one spell with a free Cantrip (0 mana, 0 XP).";
 
-    public static void SetProfession(Farmer player, MagicProfession profession)
+    public override void DoImmediateProfessionPerk()
     {
-        player.modData[ProfessionKey] = ((int)profession).ToString();
-    }
-
-    public static MagicProfession GetProfession(Farmer player)
-    {
-        if (player.modData.TryGetValue(ProfessionKey, out string? val) &&
-            int.TryParse(val, out int profInt) &&
-            Enum.IsDefined(typeof(MagicProfession), profInt))
-        {
-            return (MagicProfession)profInt;
-        }
-
-        return MagicProfession.None;
-    }
-
-    public static bool HasProfession(Farmer player, MagicProfession profession)
-    {
-        return GetProfession(player) == profession;
+        // Assign the free cantrip here
     }
 }
